23ea12b46a0b3f275ce1528c12b4f71bbb57616a
ginter <scony8877@gmail.com>
2024-05-07 17:43:55 +0800

    add space to nextlink

--- app.vue
+++ app.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <h1>Welcome</h1>
-    <NuxtLink to="/">前往 Home</NuxtLink>
-    <NuxtLink to="/about">前往 About</NuxtLink>
-    <NuxtLink to="/about/about-1">前往 About1</NuxtLink>
-    <NuxtLink to="/about/about-2">前往 About2</NuxtLink>
+    <NuxtLink class="NuxtLink" to="/">前往 Home</NuxtLink>
+    <NuxtLink class="NuxtLink" to="/about">前往 About</NuxtLink>
+    <NuxtLink class="NuxtLink" to="/about/about-1">前往 About1</NuxtLink>
+    <NuxtLink class="NuxtLink" to="/about/about-2">前往 About2</NuxtLink>
     <NuxtPage />
   </div>
 </template>
+<style>
+.NuxtLink{
+  margin-right: 20px;
+}
+</style>
