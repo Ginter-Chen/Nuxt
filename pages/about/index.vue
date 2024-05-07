6cfcad480ed4a5beb8d6de0fc6c3043b5afeb4a7
ginter <scony8877@gmail.com>
2024-05-07 16:58:18 +0800

    router file based

---
+++ pages/about/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>About Page</h1>
+    <NuxtLink to="/">前往 Home</NuxtLink>
+  </template>
+  
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Home Page</h1>
+    <NuxtLink to="/about">前往 About</NuxtLink>
+  </template>
+  
